f011fc5d208f7a6e58273da3cca315d5de5a997c
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2021-12-03 13:59:11 +0100

    Data.html

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -9,13 +9,14 @@ function checkLogin()
 
     if(username === data_username[0].username && password === data_username[0].password)
     {
-        alert('Exito');
+        sessionStorage.setItem("username", username);
         text_login.textContent = "";
+        window.location.href = 'data.html';
     }
     else
     {
-        alert('No');
         text_login.textContent = "Usuario y contraseña no válidos.";
+        sessionStorage.clear();
     }
 
     event.preventDefault();
