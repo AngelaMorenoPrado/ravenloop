d51fd877bbf0733e0aa3e2d7bbe7143a2de5a63b
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2021-12-04 14:01:37 +0100

    Tabla y modales

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -27,3 +27,51 @@ function cerrarSesion()
     sessionStorage.clear();
     window.location.href = 'index.html';
 }
+
+function myFunction(x)
+{
+    x.classList.toggle("change");
+    var input = document.getElementById('btn-menu');
+    var label_input = document.getElementById('label-input');
+    var menu = document.getElementById('nav-menu');
+
+    if(label_input.classList.contains('change'))
+    {
+        menu.style.transform = "translateX(0%)";
+        menu.style.width = "30%";
+        menu.style.zIndex = "0";
+    
+        input.style.zIndex = "3";
+    }
+    else
+    {
+        menu.style.transform = "translateX(-300%)";
+        menu.style.zIndex = "3";
+    }
+}
+
+function rellenarTabla()
+{
+    var tabla = document.getElementById('body-table');
+
+    var data_malware = JSON.parse(data);
+
+    for(var i=0; i<data_malware.length; i++)
+    {
+        tabla.innerHTML += "<tr><td>"+data_malware[i].nombre+"</td><td>"+data_malware[i].hashmd5+"</td><td>"+data_malware[i].fecha+"</td><td>"+data_malware[i].actualizacion+"</td><td>"+data_malware[i].sistema+"</td><td><button class='view_malware'>Visualizar</button></td><td><button class='update_malware' onClick='update_malware("+i+")'>Actualizar</button></td><td><button class='delete_malware'>Borrar</button></td></tr>";
+    }
+}
+
+function update_malware(number)
+{
+    var modal = document.getElementById("actualizarModal");
+    modal.style.display = "block";
+
+    var cerrar_span = document.getElementById("cerrar_boton");
+
+    
+    cerrar_span.onclick = function()
+    {
+        modal.style.display = "none";
+    }
+}
